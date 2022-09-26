43729afbbd3a27309f53ed814ec78de05ddbffb1
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-09-26 10:45:19 +0400

    commit assignment

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');//added bodyparser 
+const cors = require('cors');
+const bcrypt=require('bcrypt');
+app.use(cors());
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+
+const signuprouter=require('./src/routes/singuprouter')
+const bookrouter=require('./src/routes/bookrouter')
+
+app.use('/user',signuprouter);
+app.use('/books',bookrouter);
+
+const PORT=3000;
+app.listen(PORT,()=>console.log("server is ready at 3000"))
